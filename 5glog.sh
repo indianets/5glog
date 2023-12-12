2dc6872ae06c03032e508d2cd62cbc6d50dfcfe9
Vijay <vijay@indianets.com>
2023-12-12 20:33:15 +0530

    add TAC, PAC tracking

--- 5glog.sh
+++ 5glog.sh
@@ -13,7 +13,7 @@ newlines="$(comm -13 radio-dump.old radio-dump)"
 test -z "$newlines" && exit 0
 
 # Process new log lines to output
-test -e output.csv || echo "DATE,TIME,ARFCN,BAND,MCC,MNC,-,RSRP,RSRQ,SiNR" >output.csv
+test -e output.csv || echo "DATE,TIME,ARFCN,BAND,MCC,MNC,-,TAC,PCI,-,RSRP,RSRQ,SiNR" >output.csv
 
 lastline="$(tail -1 radio-dump.old)"
 echo "$newlines" | while read -r line; do
@@ -23,6 +23,8 @@ echo "$newlines" | while read -r line; do
     echo "Parsing unique values.."
     xdate="$(echo $line | cut -d' ' -f1)"
     xtime="$(echo $line | cut -d' ' -f2 | cut -d'.' -f1)"
+    pci="$(echo $line | sed -n -E 's/^.* mPci = (\S*) .*/\1/p')"
+    tac="$(echo $line | sed -n -E 's/^.* mTac = (\S*) .*/\1/p')"
     arfcn="$(echo $line | sed -n -E 's/^.* mNrArfcn = (\S*) .*/\1/p')"
     band="$(echo $line | sed -n -E 's/^.* mBands = (\S*) .*/\1/p')"
     mcc="$(echo $line | sed -n -E 's/^.* mMcc = (\S*) .*/\1/p')"
@@ -30,7 +32,7 @@ echo "$newlines" | while read -r line; do
     rsrp="$(echo $line | sed -n -E 's/^.* ssRsrp = (\S*) .*/\1/p')"
     rsrq="$(echo $line | sed -n -E 's/^.* ssRsrq = (\S*) .*/\1/p')"
     sinr="$(echo $line | sed -n -E 's/^.* ssSinr = (\S*) .*/\1/p')"
-    echo "$xdate,$xtime,$arfcn,$band,$mcc,$mnc,,$rsrp,$rsrq,$sinr" | tee -a output.csv
+    echo "$xdate,$xtime,$arfcn,$band,$mcc,$mnc,,$tac,$pci,,$rsrp,$rsrq,$sinr" | tee -a output.csv
   fi
   lastline="$line"
 done
