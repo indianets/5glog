1e0ee3f14ff21b44b000c8dde4db84132861c45e
Vijay <vijay@indianets.com>
2023-12-17 17:56:44 +0530

    5glive: add trends

--- 5glive.sh
+++ 5glive.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Copyright: @indianets
+# Usage: ./5glive.sh [interval]
 
 # Check for adb device
 if [ ! "$(adb devices | tail -n+2 | grep device)" ]; then
@@ -6,6 +8,7 @@ if [ ! "$(adb devices | tail -n+2 | grep device)" ]; then
   exit 1
 fi
 
+# Clear lines
 cls() {
   tput cuu1
   tput el
@@ -19,6 +22,7 @@ cls() {
   tput el
 }
 
+# Parse values and print
 main() {
   line="$(adb logcat -b radio -d | grep 'CellSignalStrengthNr' | grep 'processCellInfo' | tail -1)"
   xdate="$(echo $line | cut -d' ' -f1-2)"
@@ -29,16 +33,52 @@ main() {
   rsrq="$(echo $line | sed -n -E 's/^.* ssRsrq = (\S*) .*/\1/p')"
   sinr="$(echo $line | sed -n -E 's/^.* ssSinr = (\S*) .*/\1/p')"
 
+  if [[ "$oldrsrp" ]]; then
+    trend="$(($oldrsrp - $rsrp))"
+    rsrptrend=""
+    if [[ "$trend" -gt 0 ]]; then
+      rsrptrend="👎"
+    elif [[ "$trend" -lt 0 ]]; then
+      rsrptrend="👍"
+    fi
+  fi
+
+  if [[ "$oldrsrq" ]]; then
+    trend="$(($oldrsrq - $rsrq))"
+    rsrqtrend=""
+    if [[ "$trend" -gt 0 ]]; then
+      rsrqtrend="👎"
+    elif [[ "$trend" -lt 0 ]]; then
+      rsrqtrend="👍"
+    fi
+  fi
+
+  if [[ "$oldsinr" ]]; then
+    trend="$(($oldsinr - $sinr))"
+    sinrtrend=""
+    if [[ "$trend" -gt 0 ]]; then
+      sinrtrend="👎"
+    elif [[ "$trend" -lt 0 ]]; then
+      sinrtrend="👍"
+    fi
+  fi
+
   cls
   echo -e "TIME: $xdate\n"
-  echo -e "BAND:\t$band\t\tRSRP:\t$rsrp"
-  echo -e "TAC:\t$tac\t\tRSRQ:\t$rsrq"
-  echo -e "PCI:\t$pci\t\tSiNR:\t$sinr"
+  printf "BAND: %4s\tRSRP: %4d %s\n" "$band" "$rsrp" "$rsrptrend"
+  printf "TAC : %4d\tRSRQ: %4d %s\n" "$tac" "$rsrq" "$rsrqtrend"
+  printf "PCI : %4d\tSiNR: %4d %s\n" "$pci" "$sinr" "$sinrtrend"
+  oldrsrp="$rsrp"
+  oldrsrq="$rsrq"
+  oldsinr="$sinr"
   sleep ${1:-.5}
 }
 
 clear
-echo -e "\n\n\n\n"
+echo -e "================================"
+echo -e "\tSIGNAL STRENGTH"
+echo -e "================================"
+echo -e "\n\n\n\n\n"
 while (true); do
   main
 done
